df24c0fc8b60cc8b277340d5a563b33033776822
brandon flowers <brandonflowers@gmail.com>
2012-12-21 12:49:25 -0500

    deleted build folder

--- build/main-ck.js
+++
@@ -1,2 +0,0 @@
-// Author: Brandon Flowers / www.headwinds.net / @headwinds
-require.config({shim:{easel:{exports:"createjs"},tween:{deps:["easel"],exports:"Tween"}},paths:{jquery:"libs/jquery/require-jquery",underscore:"libs/underscore/underscore-min",backbone:"libs/backbone/backbone-min",box2d:"libs/box2d/Box2dWeb-easeljs",easel:"libs/createjs/easeljs-NEXT.min",tween:"libs/createjs/tweenjs-0.3.0.min",text:"libs/require/text",router:"routes/MainRouter",templates:"../templates"}});require(["BodyShopGameShell"],function(e){e.initialize()});
